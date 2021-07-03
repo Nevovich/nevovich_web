875f60797ca29b7e49163452e729247e93b08b36
Nevovich <60783223+Nevovich@users.noreply.github.com>
2021-07-03 20:23:52 +0600

    Nevovich: общая оптимизация
    Разработана структура модальных всплывающих окон, подключение этих окон к необходимым элементам. Оптимизация раздела "профессиональные навыки", мелкие замечания

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
-    
+    $('.header__nav a[href^="#"]').click(function (){
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 800);
+        return false;
+    });
 });
